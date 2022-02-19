717a536d1f30960d20c4205545e361af076cdb2b
Viplavraja <viplavraja2@gmail.com>
2022-02-20 01:02:16 +0530

    Template dynamic array.

--- Data-Structures/TDynamicArray.cpp
+++ Data-Structures/TDynamicArray.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <assert.h>
 using namespace std;
+
+template <class T>
 class DynamicArray
 {
 private:
     int size_;
     int max_;
-    int *arrayHolder_;
+    T *arrayHolder_;
 
 public:
     DynamicArray()
     {
         this->size_ = 0;
         this->max_ = 5;
-        this->arrayHolder_ = new int[this->max_];
+        this->arrayHolder_ = new T[this->max_];
     }
 
     ~DynamicArray()
@@ -26,30 +28,30 @@ public:
         return this->size_;
     }
 
-    int &operator[](int index)
+    T &operator[](int index)
     {
         assert(index < this->size_);
         return this->arrayHolder_[index];
     }
 
-    void add(int number)
+    void add(T value)
     {
         if (this->max_ < this->size_ + 1)
         {
             this->max_ *= 2;
-            int *tmp = new int[this->max_];
+            int *tmp = new T[this->max_];
             for (size_t i = 0; i < this->size_; i++)
             {
                 tmp[i] = this->arrayHolder_[i];
             }
             delete[] this->arrayHolder_;
             this->arrayHolder_ = tmp;
-            this->arrayHolder_[this->size_] = number;
+            this->arrayHolder_[this->size_] = value;
             this->size_ += 1;
         }
         else
         {
-            this->arrayHolder_[this->size_] = number;
+            this->arrayHolder_[this->size_] = value;
             this->size_ += 1;
         }
     }
@@ -57,13 +59,14 @@ public:
 
 int main(int argc, char **argv)
 {
-    DynamicArray dArray;
-    cout << "Addding 20 elements in Array" << endl;
-    for (size_t i = 0; i < 20; i++)
+    DynamicArray<int> dArray;
+    cout << "" << endl;
+    cout << "Addding 25 integer in Array" << endl;
+    for (size_t i = 0; i < 25; i++)
     {
         dArray.add(i);
     }
-    cout << "Last element : " << dArray[19] << endl;
+    cout << "Last element : " << dArray[24] << endl;
     cout << "Array size_ : " << dArray.getSize() << endl;
     return 0;
 }
